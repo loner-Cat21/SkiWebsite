29dbe667a86846236570e9cf49cd43219555efd9
unknown <PS24PIngram@efcts.us>
2023-03-29 10:31:47 -0400

    daagff

--- events.js
+++ events.js
@@ -1,25 +1,61 @@
  
  //Global Variables
- const btnTrails = document.getElementById('TrailGet').addEventListener('click', EventHandlerTrails);
-
- const btnMount = document.getElementById('MountRe').addEventListener('click', EventHandlerMount);
 
 
- const El_Price=document.getElementById("Price");
- let BasePrice =0;
-
-function EventHandlerTrails(){
-    document.getElementById("Price").textContent=40;
-};
-function EventHandlerMount(){
-    TrailsCalculations()
-};
-
-
-function TrailsCalculations(){
-    BasePrice="hello";
-    document.getElementById("Price").textContent=50;
-}
+let BasePrice =0;
 let details = document.getElementById("Details");
-details.textContent="44"+BasePrice;
+let StartDate;
+let endDate
+
+// Event functions
+function RadioClicked(BP){
+   BasePrice = BP;
+}
+function startDateEvent(){
+    StartDate = document.getElementById("startDate").value;
+}
+function endDateEvent(){
+    endDate = document.getElementById("endDate").value;
+}
+// function MathForPrice(){
+
+// }
+// function valadation(){
+
+// }
+function dateCreator(TempDate){
+let DateYear = TempDate.substring(0,4);
+let DateMonth = TempDate.substring(5,7);
+let DateDay = TempDate.substring(8,10);
+return new Date(DateYear,DateMonth,DateDay);
+};
+function DateCal(){
+let startdatecal =new Date(dateCreator(StartDate));
+let endDateCal = new Date(dateCreator(endDate));
+let AmountOfDays = (endDateCal -startdatecal)/(24*60*60*1000) ;
+let discountDayCount = 0;
+    let tempDate=Date.endDateCal();
+    let TempWeekDay=FindDayOfWeek(tempDate);
+    if(TempWeekDay=="Thursday" ||TempWeekDay =="Tuesday" ||TempWeekDay=="Wednesday" ){
+        discountDayCount++;
+    }
+return discountDayCount;
+};
+
+
+function FindDayOfWeek(Date){
+let ListOfDayNames =["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+return ListOfDayNames[Date];
+
+};
+function PriceFuntion(){
+    document.getElementById('Price').textContent= BasePrice;
+    document.getElementById("Details").textContent = DateCal();
+};
+
+
+
+
+
+
 
