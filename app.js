9ec00ec3d7e2ae0602d333412b24af9de8331299
Susmita Adhikari <susmitaadhikari2006@gmail.com>
2023-04-10 09:50:59 -0400

    sdffd

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ function closeNav() {
 }
 
 function getInfo() {
-  fetch( "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Colorado?unitGroup=us&key=GCXRTVTJ8DZ48XCLEYHDEPRTR&contentType=json")
+  fetch( "https://api.openweathermap.org/data/2.5/forecast?q=colorado&appid=c8b260397fd294145748ea4206128b3f")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => alert("something went wrong: Please check your internet connection"))
 
 }
